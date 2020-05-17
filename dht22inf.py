e3a4eed427c0acbb0915eae36565ec1cbb158cd3
Raul Gonzalez <ragonlan@gmail.com>
2020-05-17 23:41:27 +0200

    new json output

--- dht22inf.py
+++ dht22inf.py
@@ -10,14 +10,16 @@ import argparse
 import logging
 import pprint
 import re
+import json
 
 FORMAT = '%(asctime)-15s %(name)s [%(process)d] %(levelname)s: %(message)s'
 
-frequency = 8 #seconds. Never should be less than 2 seconds.
+frequency = 8  # seconds. Never should be less than 2 seconds.
 
-sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
-                '2302': Adafruit_DHT.AM2302 }
+sensor_args = {'11': Adafruit_DHT.DHT11,
+               '22': Adafruit_DHT.DHT22,
+               '2302': Adafruit_DHT.AM2302}
+
 
 class PrettyLog():
     def __init__(self, obj):
@@ -37,13 +39,22 @@ def parse_args():
     )
     parser.add_argument('--tag', action='store', default=dict(), nargs='*',
                         required=False, type=str, help='Add this tag to every metric. Syntax tag=value')
-    parser.add_argument('--sensor', required=True, choices=sensor_args.keys(), default=None, help='Sensor type: 11, 23 or 2302' )
-    parser.add_argument('--pin', required=True, type=int, help='GPIO pin, for example: 4')
-    parser.add_argument('--influxdatabase', required=False, type=str, default='temperature', help='Influx Database to store temperature')
-    parser.add_argument('--influxserver', required=False, type=str, default='localhost', help='Influx server hostname')
-    parser.add_argument('--loop', required=False, action='store_true', default=False, help='Only excetue forever')
-    parser.add_argument('--frequency', required=False, type=int, default=frequency, help='Temperature/humidity reading frequency')
+    parser.add_argument('--sensor', required=True, choices=sensor_args.keys(),
+                        default=None, help='Sensor type: 11, 23 or 2302')
+    parser.add_argument('--pin', required=True, type=int,
+                        help='GPIO pin, for example: 4')
+    parser.add_argument('--influxdatabase', required=False, type=str,
+                        default='temperature', help='Influx Database to store temperature')
+    parser.add_argument('--influxserver', required=False, type=str,
+                        default='localhost', help='Influx server hostname')
+    parser.add_argument('--loop', required=False, action='store_true',
+                        default=False, help='Only excetue forever')
+    parser.add_argument('--frequency', required=False, type=int,
+                        default=frequency, help='Temperature/humidity reading frequency')
+    parser.add_argument('--json', required=False,
+                        action='store_true', default=False, help='json output')
     return parser.parse_args()
+
 
 def parseTags(tags):
     extratags = dict()
@@ -60,17 +71,20 @@ def parseTags(tags):
     # logging.info(PrettyLog(extratags))
     return extratags
 
+
 try:
     args = parse_args()
     args.sensor = sensor_args[args.sensor]
     logging.basicConfig(level=args.loglevel, format=FORMAT)
     extratags = parseTags(args.tag)
     ip = 'No IP'
+
     if ni.ifaddresses('eth0') and ni.AF_INET in ni.ifaddresses('eth0'):
         ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
     elif ni.ifaddresses('wlan0') and ni.AF_INET in ni.ifaddresses('wlan0'):
         ip = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
-    if not args.loglevel == logging.DEBUG:
+
+    if not args.loglevel == logging.DEBUG and not args.json:
         logging.debug('Debug mode, not storing data to influxdb server.')
         client = InfluxDBClient(host='localhost', database=args.influxdatabase)
     else:
@@ -93,8 +107,11 @@ try:
             }
         ]
         json_body[0]['fields'].update(extratags)
-        if not args.loglevel == logging.DEBUG:
+        if not args.loglevel == logging.DEBUG and not args.json:
             client.write_points(json_body)
+        if args.json:
+            jsondata = json.dumps(json_body[0])
+            print(jsondata)
         if (not args.loop):
             break
         sleep(args.frequency)
